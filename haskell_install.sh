5a8bd7a573a29db8c17ad2f7725f437288bf4218
Kirill Davydychev <kdavyd@gmail.com>
2013-02-09 15:32:47 -0600

    Update haskell_install.sh

--- haskell_install.sh
+++ haskell_install.sh
@@ -10,10 +10,11 @@ cd /var/tmp/haskell
 apt-get update
 apt-get install gcc libgmp*
 wget http://www.haskell.org/ghc/dist/6.6/ghc-6.6-i386-unknown-solaris2.tar.bz2
-ghc-6.6-i386-unknown-solaris2.tar.bz2
+tar xvf ghc-6.6-i386-unknown-solaris2.tar.bz2
 cd ghc-6.6
 ./configure
 make install
+cd
 rm -rf /var/tmp/haskell
 ghc -V
 echo "Done."
