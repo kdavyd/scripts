2bdb052d057351b0c4d646550195e070a574fea2
Kirill Davydychev <kdavyd@gmail.com>
2014-06-27 14:18:28 -0500

    Create cfgadm.sh
    Workaround for NEX-2074 for FC initiator systems with non-FCAL targets. Replace /usr/sbin/cfgadm with this script; move original cfgadm to cfgadm.orig

---
+++ cfgadm.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/bash
+
+if [ "$1" == "-c" ]
+then
+	if [ "$2" == "unconfigure" ]
+	then 
+		c_type="`cfgadm.orig $3 | tr -s " " | tail -1 | cut -d " " -f 2`"
+		if [ "$c_type" == "fc-fabric" ]
+		then 
+			date >> /var/log/cfgadm_hack.log
+			echo "Would've unconfigured $c_type device $3" >> /var/log/cfgadm_hack.log
+		else 
+			if [ "$c_type" == "fc-public" ]
+			then 
+				date >> /var/log/cfgadm_hack.log
+				echo "Would've unconfigured $c_type device $3" >> /var/log/cfgadm_hack.log
+			else
+				if [ "$c_type" == "raid/hp" ]
+				then
+					date >> /var/log/cfgadm_hack.log
+					echo "Would've unconfigured $c_type device $3" >> /var/log/cfgadm_hack.log
+				else
+					cfgadm.orig $1 $2 $3 $4
+				fi
+			fi
+		fi
+	else 
+		cfgadm.orig $1 $2 $3 $4
+	fi
+else
+	cfgadm.orig $1 $2 $3 $4
+fi
