057972dd1c963f9288f58755ee990b432dda2e4b
Kirill Davydychev <kdavyd@gmail.com>
2017-05-10 11:59:54 -0700

    Update centos_kdprep.sh

--- centos_kdprep.sh
+++ centos_kdprep.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 yum install -y nano perf
 mkdir ~/cosbench && cd ~/cosbench && wget https://github.com/intel-cloud/cosbench/releases/download/v0.4.2.c4/0.4.2.c4.zip && unzip 0.4.2.c4.zip
