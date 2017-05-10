dcfdd64ee36df585448fbd864536d71b05a24529
Kirill Davydychev <kdavyd@gmail.com>
2017-05-10 12:03:42 -0700

    added pssh

--- centos_kdprep.sh
+++ centos_kdprep.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 yum install -y nano perf
+yum install epel-release -y
+yum install -y pssh
 mkdir ~/cosbench && cd ~/cosbench && wget https://github.com/intel-cloud/cosbench/releases/download/v0.4.2.c4/0.4.2.c4.zip && unzip 0.4.2.c4.zip
 chmod +x 0.4.2.c4/start-all.sh 
 chmod +x 0.4.2.c4/stop-all.sh
