ac597e1c3fa86c8ab503003b928e0c1b76df14a5
Kirill Davydychev <kdavyd@gmail.com>
2017-05-25 15:14:41 -0500

    added screen

--- centos_kdprep.sh
+++ centos_kdprep.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-yum install -y nano perf atop lsscsi mosh
+yum install -y nano perf atop lsscsi mosh screen
 yum install epel-release -y
 yum install -y pssh
 mkdir ~/cosbench && cd ~/cosbench && wget https://github.com/intel-cloud/cosbench/releases/download/v0.4.2.c4/0.4.2.c4.zip && unzip 0.4.2.c4.zip
