a4d79554402716b8436e6e372d5e0f4a4f77ad1d
Kirill Davydychev <kdavyd@gmail.com>
2017-06-06 15:37:49 -0500

    Update centos_kdprep.sh

--- centos_kdprep.sh
+++ centos_kdprep.sh
@@ -4,6 +4,7 @@ yum install -y nano perf lsscsi screen
 yum install epel-release -y
 yum install -y pssh atop mosh cloud-utils-growpart python-pip
 pip install --upgrade --user awscli
+ln -s /root/.local/bin/aws /usr/bin
 mkdir ~/cosbench && cd ~/cosbench && wget https://github.com/intel-cloud/cosbench/releases/download/v0.4.2.c4/0.4.2.c4.zip && unzip 0.4.2.c4.zip
 chmod +x 0.4.2.c4/start-all.sh 
 chmod +x 0.4.2.c4/stop-all.sh
