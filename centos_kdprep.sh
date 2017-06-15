4fbf1cb14fa7b7878ab452a18821e135be35a526
Kirill Davydychev <kdavyd@gmail.com>
2017-06-14 21:22:36 -0700

    Create centos_kdprep.sh

--- centos_kdprep.sh
+++ centos_kdprep.sh
@@ -6,5 +6,7 @@ yum install -y pssh atop mosh cloud-utils-growpart python-pip wget tmux
 pip install --upgrade --user awscli
 ln -s /root/.local/bin/aws /usr/bin
 mkdir ~/cosbench && cd ~/cosbench && wget https://github.com/intel-cloud/cosbench/releases/download/v0.4.2.c4/0.4.2.c4.zip && unzip 0.4.2.c4.zip
-chmod +x 0.4.2.c4/start-all.sh 
+chmod +x 0.4.2.c4/start-all.sh
+chmod +x 0.4.2.c4/start-driver.sh
+chmod +x 0.4.2.c4/start-controller.sh
 chmod +x 0.4.2.c4/stop-all.sh
