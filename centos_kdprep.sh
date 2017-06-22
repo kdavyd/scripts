81f2e9820d1cc401b12d2333377c319020c28853
Kirill Davydychev <kdavyd@gmail.com>
2017-06-22 00:24:15 -0700

    made sure tiptop builds on more centos 6 systems

--- centos_kdprep.sh
+++ centos_kdprep.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 yum install epel-release -y
-yum install -y pssh atop mosh cloud-utils-growpart python-pip wget tmux nano perf lsscsi screen expect expect-devel papi-devel   
+yum install -y pssh atop mosh cloud-utils-growpart python-pip wget tmux nano perf lsscsi screen expect expect-devel papi-devel gcc byacc flex
 pip install --upgrade --user awscli
 ln -s /root/.local/bin/aws /usr/bin
 mkdir ~/cosbench && cd ~/cosbench && wget https://github.com/intel-cloud/cosbench/releases/download/v0.4.2.c4/0.4.2.c4.zip && unzip 0.4.2.c4.zip
@@ -9,4 +9,4 @@ chmod +x 0.4.2.c4/start-all.sh
 chmod +x 0.4.2.c4/start-driver.sh
 chmod +x 0.4.2.c4/start-controller.sh
 chmod +x 0.4.2.c4/stop-all.sh
-cd && git clone https://github.com/FeCastle/tiptop.git && ./configure && make && make install clean && cd
+cd && git clone https://github.com/FeCastle/tiptop.git && cd tiptop && ./configure && make && make install clean && cd
